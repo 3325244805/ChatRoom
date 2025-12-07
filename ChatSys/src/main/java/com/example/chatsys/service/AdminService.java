e16562369b63eab402003919c4279d1eb5181163
Shen <155442380+3325244805@users.noreply.github.com>
2025-12-07 23:00:01 +0800

    Delete ChatSys

--- ChatSys/src/main/java/com/example/chatsys/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.example.chatsys.service;
-
-import com.example.chatsys.entity.Admin;
-
-public interface AdminService {
-    Admin findByAdminName(String adminName);
-
-    Admin findByEmail(String email);
-}
